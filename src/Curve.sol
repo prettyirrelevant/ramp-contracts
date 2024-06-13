3e033ab07bce0059c77be8103475716b4856fe9d
Emmanuel Oluwatobi <56241351+Tee-py@users.noreply.github.com>
2024-06-13 09:08:35 +0100

    feat: add swap support for migrated token liquidity

--- src/Curve.sol
+++ src/Curve.sol
@@ -75,32 +75,84 @@ contract BondingCurveAMM {
         return (summation * 1 ether) / 16000;
     }
 
-    function buyToken(address token, uint256 amount) public payable {
-        // Todo: reroute the call to the fraxswap pool instead
-        require(tokenReserve[token] < reserveTarget, "curve target reached");
-        uint256 supply = Token(token).totalSupply();
-        uint256 price = getPrice(supply, amount);
-        uint256 protocolFee = (price * protocolFeePercent) / 100_00;
-        require(msg.value >= price + protocolFee, "Insufficient payment for trade");
-        tokenReserve[token] += price;
+    function swapETHForToken(address token, uint256 amountIn, uint256 amountOutMin, address to) internal returns (uint256, uint256) {
+        require(msg.value >= amountIn, "insufficient eth provided for swap");
+        uint256 protocolFee = (amountIn * protocolFeePercent) / 100_00;
+        address[] memory path = new address[](2);
+        path[0] = address(0);
+        path[1] = token;
+        uint[] memory amounts = swapRouter.swapExactETHForTokens{value: amountIn - protocolFee}(
+            amountOutMin,
+            path,
+            to,
+            block.timestamp + 1 minutes
+        );
+        uint amountOut = amounts[amounts.length - 1];
         (bool success, ) = protocolFeeRecipient.call{ value: protocolFee }("");
-        Token(token).mintTo(msg.sender, amount);
         require(success, "Unable to send protocol fee");
-        emit Trade(msg.sender, token, msg.value, amount, protocolFee, block.timestamp, true);
+        return (protocolFee, amountOut);
     }
 
-    function sellToken(address token, uint256 amount) public {
-        // If the token has been migrated to fraxSwap, route the call to fraxSwap instead
-        uint256 supply = Token(token).totalSupply();
-        require(Token(token).balanceOf(msg.sender) >= amount, "insufficient balance to cover trade");
-        uint256 price = getPrice(supply - amount, amount);
-        uint256 protocolFee = (price * protocolFeePercent) / 100_00;
-        tokenReserve[token] -= price;
-        Token(token).burnFrom(msg.sender, amount);
-        (bool success1, ) = msg.sender.call{ value: price - protocolFee }("");
+    function swapTokenForETH(address token, uint256 amountIn, uint256 amountOutMin, address to) internal returns (uint256, uint256) {
+        address[] memory path = new address[](2);
+        path[0] = address(0);
+        path[1] = token;
+        uint[] memory amounts = swapRouter.swapExactTokensForETH(
+            amountIn, 
+            amountOutMin, 
+            path,
+            address(this), 
+            block.timestamp + 1 minutes
+        );
+        uint amountOut = amounts[amounts.length - 1];
+        uint256 protocolFee = (amountOut * protocolFeePercent) / 100_00;
+        (bool success1,) = to.call{ value: amountOut - protocolFee}("");
         (bool success2, ) = protocolFeeRecipient.call{ value: protocolFee }("");
-        require(success1 && success2, "Unable to send funds");
-        emit Trade(msg.sender, token, amount, price - protocolFee, protocolFee, block.timestamp, false);
+        require(success1 && success2, "unable to send funds");
+        return (protocolFee, amountOut - protocolFee);
+    }
+
+    function buyToken(address token, uint256 amount) public payable {
+        uint256 fee = 0;
+        uint256 amountOut = 0;
+        if (isLiquidityAdded[token]) {
+            (fee, amountOut) = swapETHForToken(token, msg.value, amount, msg.sender);
+        } else {
+            require(tokenReserve[token] < reserveTarget, "curve target reached");
+            uint256 supply = Token(token).totalSupply();
+            uint256 price = getPrice(supply, amount);
+            fee = (price * protocolFeePercent) / 100_00;
+            amountOut = amount;
+            require(msg.value >= price + fee, "Insufficient payment for trade");
+            tokenReserve[token] += price;
+            Token(token).mintTo(msg.sender, amount);
+        }
+        emit Trade(msg.sender, token, msg.value, amount, fee, block.timestamp, true);
+    }
+
+    function sellToken(address _token, uint256 amountIn, uint256 amountOutMin) public {
+        uint256 fee = 0;
+        uint256 amountOut = 0;
+        Token token = Token(_token);
+        if (isLiquidityAdded[_token]) {
+            bool success1 = token.transferFrom(msg.sender, address(this), amountIn);
+            bool success2 = token.approve(address(swapRouter), amountIn);
+            require(success1 && success2, "failed to move tokens");
+            (fee, amountOut) = swapTokenForETH(_token, amountIn, amountOutMin, msg.sender);
+        } else {
+            uint256 supply = token.totalSupply();
+            require(token.balanceOf(msg.sender) >= amountIn, "insufficient balance to cover trade");
+            uint256 price = getPrice(supply - amountIn, amountIn);
+            uint256 protocolFee = (price * protocolFeePercent) / 100_00;
+            tokenReserve[_token] -= price;
+            Token(token).burnFrom(msg.sender, amountIn);
+            (bool success1, ) = msg.sender.call{ value: price - protocolFee }("");
+            (bool success2, ) = protocolFeeRecipient.call{ value: protocolFee }("");
+            require(success1 && success2, "Unable to send funds");
+            fee = protocolFee;
+            amountOut = price - fee;
+        }
+        emit Trade(msg.sender, _token, amountIn, amountOut, fee, block.timestamp, false);
     }
 
     function launchToken(TokenLaunchParam memory param) public {
@@ -125,7 +177,8 @@ contract BondingCurveAMM {
         uint256 ethAmount = tokenReserve[_token];
         uint256 supply = token.totalSupply();
         token.mintTo(address(this), supply);
-        token.approve(address(swapRouter), supply);
+        bool success = token.approve(address(swapRouter), supply);
+        require(success, "token approval failed");
         swapRouter.addLiquidityETH{ value: ethAmount }(
             _token,
             supply,
